d853b67af84b9871e190222c2ba13874c2cd3c40
Jonas R <jonas.reinhardt@student.fh-kiel.de>
2021-06-07 10:41:37 +0200

    added repetition for wordcount

--- Code-Vorlesungen/Wiederholung/Wiederholung-Listen-Strings-Dictionaries.py
+++ Code-Vorlesungen/Wiederholung/Wiederholung-Listen-Strings-Dictionaries.py
@@ -1,12 +1,21 @@
-
 a = "Das ist ein String"
 b = ["Das", "ist", "eine", "Liste"]
 c = ["Mehrere", "Datentypen", 5.3, True]
 d = {"Berlin": 3_500_000,
      "Kiel": 255_000}
 
-# Etwas zu einer Liste hinzufügen
+# Einen String mithilfe von split() in eine Liste überführen
+a_splitted = a.split(" ")
+# Mithilfe von len() die Länge der Liste ermitteln
+print(len(a_splitted))
+# Ermitteln der durchschnittlichen Wortlänge
+wortlaenge = len(a) / len(a_splitted)
+print("Die Wörter sind durchschnittlich", wortlaenge, "Zeichen lang.")
+
+# Verschachtelung zu einer Liste hinzufügen
 print(b)
 b.append(["Liste", "in", "der", "Liste"])
 print(b)
+# Auf ein bestimmtes Element einer Liste innerhalb einer Liste zugreifne
 print(b[4][0])
+
