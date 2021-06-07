220d5dfccbf3613da79e1063618c90a2fe26011e
Jonas R <jonas.reinhardt@student.fh-kiel.de>
2021-06-07 10:24:58 +0200

    wip

---
+++ Code-Vorlesungen/Wiederholung/Wiederholung-Listen-Strings-Dictionaries.py
@@ -0,0 +1,12 @@
+
+a = "Das ist ein String"
+b = ["Das", "ist", "eine", "Liste"]
+c = ["Mehrere", "Datentypen", 5.3, True]
+d = {"Berlin": 3_500_000,
+     "Kiel": 255_000}
+
+# Etwas zu einer Liste hinzufügen
+print(b)
+b.append(["Liste", "in", "der", "Liste"])
+print(b)
+print(b[4][0])
