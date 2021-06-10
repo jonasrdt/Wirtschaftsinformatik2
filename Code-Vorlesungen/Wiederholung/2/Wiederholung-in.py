98c92137f5717976f98227a0295f4b56e900439c
Jonas R <jonas.reinhardt@student.fh-kiel.de>
2021-06-10 10:29:50 +0200

    wip

--- Code-Vorlesungen/Wiederholung/2/Wiederholung-in.py
+++ Code-Vorlesungen/Wiederholung/2/Wiederholung-in.py
@@ -1,8 +1,18 @@
 
 warenlager = {"Schrauben": 500,
               "Nieten": 150,
-              "2m Stahlträger": 250,
-              "Schrauben": 10}
+              "2m Stahlträger": 250
+              }
+
+print(warenlager)
+warenlager.update({"Muttern": 50})
+print(warenlager)
+warenlager.update({"Schrauben": 310})
+print(warenlager)
+warenlager.pop("Schrauben")
+print(warenlager)
+warenlager.clear()
+print(warenlager)
 
 abfrage = input("Von welchem Posten wollen Sie die verfügbare Anzahl im Lager wissen: ")
 
