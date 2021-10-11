8c5e80cd1b631ec038bf8da097f7442213ad587a
Jonas R <jonas.reinhardt@student.fh-kiel.de>
2021-10-11 08:28:11 +0200

    added extended brutto-netto-calc

---
+++ Code-Vorlesungen/Wiederholung/2/test.py
@@ -0,0 +1,47 @@
+# adressbuch = {"Lara": 123456}
+
+# adressbuch["Klaus"] = 123456
+# print(adressbuch.items(0))
+
+
+
+# def a():
+#     count = 0
+#     count += 0
+#     for z in (1,2,4):
+#         count +=1
+#     return count
+
+# print (a())
+
+
+# a = 11
+# def kl(a):
+#  a -= 3
+#  return a
+# print(kl(a))
+# print(a)
+
+
+
+# a = 4
+# def g(var):
+#     a = 16
+#     return var - 1
+# def f(var):
+#     a = 25
+#     return g(var * 2)
+# print(f(a))
+
+
+# myList = ['5', '9', '6']
+# myList.append(['1', '5'])
+# print(myList)
+# print(len(myList))
+
+def a():
+    count = 0
+    for z in (100,2,3):
+        count += 1
+    return count
+print(a())
