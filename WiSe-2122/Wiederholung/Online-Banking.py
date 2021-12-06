2c49f48f9835bd25059a8ce7e4a210422d0d7fcf
Jonas R <jonas.reinhardt@student.fh-kiel.de>
2021-12-06 09:56:48 +0100

    expanded the online banking exmaple

--- WiSe-2122/Wiederholung/Online-Banking.py
+++ WiSe-2122/Wiederholung/Online-Banking.py
@@ -4,6 +4,7 @@
 # Außerdem sollen Fehleingaben durch den Nutzer abgefangen werden.
 
 richtige_eingabe = False
+kontostand = 2500
 
 def trenner(anzahl):
     for zaehler in range(anzahl):
@@ -12,14 +13,16 @@ def trenner(anzahl):
 
 trenner(50)
 print("Willkommen im Online-Banking der DKB.")
+print("Ihr aktueller Kontostand beträgt: ", kontostand, "€.")
 while not richtige_eingabe:
     try:
         betrag = float(input("Bitte geben Sie einen Überweisungsbetrag an: "))
         trenner(50)
-        if betrag > 0 and betrag <= 50_000:
+        if betrag > 0 and betrag <= 50_000 and betrag <= kontostand:
             print("Der Betrag i.H.v.", betrag, "€ kann überwiesen werden.")
+            print("Ihr neuer Kontostand beträgt:", kontostand - betrag, "€.")
             richtige_eingabe = True
         else:
-            print("Bitte geben Sie einen gültigen Betrag ein, der größer als 0 aber kleiner als 50.000 ist.")
+            print("Bitte geben Sie einen gültigen Betrag ein, der größer als 0 aber kleiner als 50.000 ist und Ihren Kontostand i.H.v", kontostand, "€ nicht übersteigt.")
     except:
         print("Bitte geben Sie nur gültige Fließkommawerte an.")
