1f7c8a7e16eb6970424bd8335e4319e31ed604bd
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-03-23 11:13:19 +0100

    added new exkurs

--- Uebung-1/uebung1.py
+++ Uebung-1/uebung1.py
@@ -4,3 +4,10 @@
 a = 5               # Integer (int), alle ganzen Zahlen
 b = 5.5             # Float (float), alle Fließkommazahlen
 c = "Jonas123"      # String (str), alle Zeichen(ketten)
+
+vorname = input("Bitte geben Sie Ihren Vornamen ein: ")
+print("Ihr Vorname lautet: ", vorname)
+
+alter = input("Bitte geben Sie Ihr Alter ein: ")
+print("Ihr Alter lautet: ", alter)
+
