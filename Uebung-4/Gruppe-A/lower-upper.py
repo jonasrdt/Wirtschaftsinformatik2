72932a56aabe7dde1aa9ec2e80f4aedcf2505a62
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-04-06 10:48:57 +0200

    added lower upper example for group A

---
+++ Uebung-4/Gruppe-A/lower-upper.py
@@ -0,0 +1,12 @@
+# Beispiele für Manipulation von Zeichnketten (str)
+# print("lowercase", familienstand.lower())
+# print("uppercase", familienstand.upper())
+# print("capitalize", familienstand.capitalize())
+
+
+familienstand = input("Bitte geben Sie Ihren Familienstand an: ")
+if familienstand.lower() == "ledig":
+    print("Willkommen bei Tinder")
+else:
+    print("Herzlichen Glückwunsch.")
+    
