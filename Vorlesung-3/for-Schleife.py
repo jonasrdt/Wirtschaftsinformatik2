16cc822c459970624ae68b7ea07389f0416b37a7
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-04-06 10:39:20 +0200

    added for loop

---
+++ Vorlesung-3/for-Schleife.py
@@ -0,0 +1,17 @@
+
+# Folgende verschachtelte Schleife geht in die erste Zeile
+# geht dann alle Spalten durch und anschließend in die nächste Zeile
+# um den Vorgang zu wiederholen.
+
+# for zeile in range(1,5):
+#     for spalte in range(1,15):
+#         print("*", end=" ")
+#     print()
+
+
+if 4 <= 4:
+    print("A")
+if 3 < 4:
+    print("B")
+else:
+    print("C")
