7021b3cb9a93bc5387376f85e53baefbaebc3733
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-05-18 08:57:05 +0200

    added list example

---
+++ Vorlesung-5/Listen-Einfuehrung.py
@@ -0,0 +1,15 @@
+
+
+# to-do Liste
+todo = []
+
+print("Auf der to-do Liste steht:", todo)
+weiteres_element = True
+while weiteres_element:
+    element = input("Bitte geben Sie etwas ein: ")
+    todo.append(element)
+    print("Auf der to-do Liste steht:", todo)
+    entscheidung = input("Wollen Sie noch etwas hinzufügen (ja/nein): ")
+    if entscheidung.lower() == "nein":
+        weiteres_element = False
+print("Sie haben folgende Elemente auf der Liste erfasst:", todo)
