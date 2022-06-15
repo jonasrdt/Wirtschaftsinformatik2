51dd7641e00f73c3b705425894e2aa369b8406e8
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-06-15 09:25:52 +0200

    refactored dictionary repitition with functions

--- Vorlesung-9/Wiederholung-Dictionary.py
+++ Vorlesung-9/Wiederholung-Dictionary.py
@@ -2,24 +2,43 @@
 # Dictionary ist ein Key Value Store
 todo = {}
 
+
+# Funktionsdefinition
+def wichtigkeit():
+    """
+    The function asks the user to input the importance of the task. The input is then added to the
+    dictionary as a value
+    """
+    todo_wichtigkeit = input("Bitte geben Sie die Wichtigkeit ein (Wichtig/Mittelwichtig/Unwichtig): ")
+    todo.update({todo_eintrag.capitalize(): todo_wichtigkeit}) 
+
+def schleifenabbruch():
+    """
+    It asks the user if they want to write another entry. If they say no, it returns False
+    :return: the boolean value False.
+    """
+    entscheidung = input("Wollen Sie einen weiteren Eintrag verfassen (ja/nein): ")
+    if entscheidung.lower() == "nein":
+        return False
+    if entscheidung.lower() == "ja":
+        return True
+
+    
+    
+########################
+##### Hauptprogramm ####
+########################
 weiterer_eintrag = True
 while weiterer_eintrag:
     todo_eintrag = input("Bitte geben Sie ein todo ein: ")
     
-    if todo_eintrag in todo:
+    if todo_eintrag.capitalize() in todo:
         ueberschreiben = input("Der Eintrag ist doppelt. Wollen Sie den bestehenden Eintrag überschreiben (ja/nein): ")
         if ueberschreiben.lower() == "ja":
-            todo_wichtigkeit = input("Bitte geben Sie die Wichtigkeit ein (Wichtig/Mittelwichtig/Unwichtig): ")
-            todo.update({todo_eintrag: todo_wichtigkeit})
+            wichtigkeit()
         else:
-            entscheidung = input("Wollen Sie einen weiteren Eintrag verfassen (ja/nein): ")
-            if entscheidung.lower() == "nein":
-                weiterer_eintrag = False
+            weiterer_eintrag = schleifenabbruch()
     else:           
-        todo_wichtigkeit = input("Bitte geben Sie die Wichtigkeit ein (Wichtig/Mittelwichtig/Unwichtig): ")
-        todo.update({todo_eintrag: todo_wichtigkeit}) 
-          
-        entscheidung = input("Wollen Sie einen weiteren Eintrag verfassen (ja/nein): ")
-        if entscheidung.lower() == "nein":
-            weiterer_eintrag = False
+        wichtigkeit()
+        weiterer_eintrag = schleifenabbruch()
 print(todo)
