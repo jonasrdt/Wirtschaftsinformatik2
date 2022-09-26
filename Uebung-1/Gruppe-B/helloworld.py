a11583e61f0f5116e96f356966bcb9bae0a8dc4a
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-09-26 16:49:49 +0200

    added first examples for group B

---
+++ Uebung-1/Gruppe-B/helloworld.py
@@ -0,0 +1,23 @@
+
+# Ausgabe eines Strings mit der print() Methode
+print("Hello World!")
+print("Hallo mein Name ist Jonas.")
+
+# Addition von zwei Zahlen des Typens Integer (int) => alle ganzen Zahlen
+print(5 + 5)
+
+# Addition von Integer (int) und Float (float) => Fließkommazahl
+print(5 + 2.5)
+
+# Subtraktion von zwei Integer Werten
+print(5 - 3)
+
+# Divison von zwei Integer Werten
+print(5 / 3)
+
+# Potenzieren eines Integer Wertes
+print(10 ** 4)
+
+# Mehrfachausgabe mithilfe der print()-Methode
+print("Jonas", 3)
+
