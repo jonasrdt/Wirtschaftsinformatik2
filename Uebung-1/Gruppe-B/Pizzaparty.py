d32983fb7cb74a790422dd96ec500fcf53897aae
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-09-26 17:11:15 +0200

    added variabledefinition and pizzaparty

---
+++ Uebung-1/Gruppe-B/Pizzaparty.py
@@ -0,0 +1,5 @@
+
+# Schreiben Sie ein Programm, mithilfe dessen eine Pizza bestellt
+# deren Menge angegeben und der Gesamtpreis berechnet werden kann.
+# Die Pizza kostet im Mittagsangebot immer 5,00€
+
---
+++ Uebung-1/Gruppe-B/Variablendefinition.py
@@ -0,0 +1,12 @@
+
+# Definition von Variablen des Datentyps String (str)
+# Es ist egal, welche Art von Anführungszeichen genutzt werden
+vorname = input("Bitte geben Sie Ihren Vornamen ein: ")
+nachname = input("Bitte geben Sie Ihren Nachnamen ein: ")
+
+# Definition einer Variable des Datentyps Integer (int)
+alter = int(input("Bitte geben Sie Ihr Alter ein: "))
+
+# Mehrfachausgabe von zwei Strings (str) und einem Integer (int).
+print(vorname, nachname , "ist", alter, "Jahre alt.")
+print("In fünf Jahren ist", vorname, alter + 5, "Jahre alt.")
