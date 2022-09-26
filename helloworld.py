7810b41a4e758a208d53bb5e87a14ad02a8d3828
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-09-26 15:06:43 +0200

    added first example in helloworld py

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+
+# Ausgabe eines Strings
+print("The Queen is dead. Long live the King.")
+
+# Addition
+print(5 + 10)
+
+# Subtraktion
+print(5 - 10)
+
+# Division
+print(5 / 2)
+
+# Multiplikation
+print(10 * 2)
+
+# Potenzieren mit dem Exponenten 4
+print(10 ** 4)
