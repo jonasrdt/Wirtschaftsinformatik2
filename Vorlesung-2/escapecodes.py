f851bc7d05f1d46c67aed834e49dc7a57b772c98
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-05 09:54:31 +0200

    added examples for lecture 2

---
+++ Vorlesung-2/escapecodes.py
@@ -0,0 +1,15 @@
+
+# Nutzung eines Backslashes in Kombination
+# mit einem Anführungszeichen für einen Escape-Code
+print("Wie geht das \"eigentlich?")
+
+# Nutzung von \t zum Einrücken eines Tabs
+print("Der Einkauf kostet: \t 127,00 €")
+print("Die Miete kostet: \t 650,00 €")
+
+
+
+
+
+      
+      
---
+++ Vorlesung-2/variablendefinition.py
@@ -0,0 +1,11 @@
+# alter = 50
+# print("Peter ist", alter, "Jahre alt.")
+
+# alter -= 1
+# print("Peter ist", alter, "Jahre alt.")
+
+# Variable vom Datentyp String (str) mit dem Inhalt "Jonas"
+vorname = "Jonas"
+
+print("Sein Vorname ist:", vorname)
+
