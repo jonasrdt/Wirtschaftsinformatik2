24da1dac3332fd686a935491051a84391041469b
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-05 10:45:36 +0200

    added mean example

---
+++ Uebung-1/Gruppe-C/mittelwert.py
@@ -0,0 +1,10 @@
+
+# Ändern Sie das Programm so ab, dass der Nutzer die drei Zahlen eingibt
+# und daraus der Mittelwert errechnet wird.
+
+erste_zahl = 5
+zweite_zahl = 3
+dritte_zahl = 7
+
+mittelwert = (erste_zahl + zweite_zahl + dritte_zahl) / 3
+print("Der Mittelwert der Zahlen:", erste_zahl, zweite_zahl, dritte_zahl, "lautet:", mittelwert)
