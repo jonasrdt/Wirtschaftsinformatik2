1f7a443ea66890209742256bc8e941189c5c9233
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-05 10:45:52 +0200

    added qis example

---
+++ Uebung-1/Gruppe-C/studierendenkartei.py
@@ -0,0 +1,15 @@
+# Variablen vom Datentypen String (str)
+vorname = "Jonas" 
+nachname = "Reinhardt"
+studiengang = "Betriebswirtschaftslehre B.A."
+# Variablen vom Datentypen Integer (int) 
+matrikelnummer = 931589
+alter = 29
+studiendauer_in_jahren = 3
+studiendauer_in_semester = studiendauer_in_jahren * 2
+# Variable vom Datentypen Float (float)
+notendurchschnitt = 2.2
+
+print("Der Name des Studierenden lautet:\t", vorname, nachname)
+print("Die Matrikelnummer lautet:\t\t", matrikelnummer)
+print("Die Studiendauer in Semestern beträgt:\t", studiendauer_in_semester)
