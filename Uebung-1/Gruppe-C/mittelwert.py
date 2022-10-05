995fd5ea5089c0057b9a8ad9fa19966987bd3719
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-05 11:02:08 +0200

    finished mean example

--- Uebung-1/Gruppe-C/mittelwert.py
+++ Uebung-1/Gruppe-C/mittelwert.py
@@ -1,10 +1,14 @@
-
 # Ändern Sie das Programm so ab, dass der Nutzer die drei Zahlen eingibt
 # und daraus der Mittelwert errechnet wird.
 
-erste_zahl = 5
-zweite_zahl = 3
-dritte_zahl = 7
+# Abfrage der Zahlen durch den User und umwandeln (casten) in den Datentypen Float (float)
+erste_zahl = float(input("Bitte geben Sie die erste Zahl ein: \t"))
+zweite_zahl = float(input("Bitte geben Sie die zweite Zahl ein: \t"))
+dritte_zahl = float(input("Bitte geben Sie die dritte Zahl ein: \t"))
 
-mittelwert = (erste_zahl + zweite_zahl + dritte_zahl) / 3
+# Berechnung des Mittelwerts auf Basis der vom User eingegebenen Zahlen
+# und runden auf zwei Nachkommastellen
+mittelwert = round((erste_zahl + zweite_zahl + dritte_zahl) / 3, 2)
+
+# Ausgabe des Mittelwertes und der vom User eingegebenen Zahlen
 print("Der Mittelwert der Zahlen:", erste_zahl, zweite_zahl, dritte_zahl, "lautet:", mittelwert)
