d980ca1702244d0da86bca9f2061d7a865d09691
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-13 11:00:11 +0200

    added few examples for wi group

---
+++ VL-Einfuehrung-Programmierung-Vertretung/kontrollstrukturen-for-schleife.py
@@ -0,0 +1,11 @@
+
+# Gefüllte Hähnchenroulade mit Schafskäse
+# Zwei Zwiebeln müssen geschnitten werden
+
+anzahl_zwiebeln = 2
+
+for zwiebel in range(anzahl_zwiebeln):
+    print("Zwiebel Nummer", zwiebel, "aus dem Schrank nehmen.")
+    print("Ich schneide und weine und schneide und weine ... ")
+    anzahl_zwiebeln -= 1
+    print("Es müssen noch", anzahl_zwiebeln, "geschnitten werden.")
