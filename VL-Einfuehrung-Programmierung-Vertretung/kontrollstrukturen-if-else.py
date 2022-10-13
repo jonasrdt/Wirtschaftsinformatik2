500b4ce7056ba849575327f966220eb96a8ad557
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-13 11:00:31 +0200

    added few examples for wi group

---
+++ VL-Einfuehrung-Programmierung-Vertretung/kontrollstrukturen-if-else.py
@@ -0,0 +1,9 @@
+alter = int(input("Bitte geben Sie Ihr Alter ein: "))
+
+# Prüfen, ob die Abfrage True ist...
+if alter > 16:
+    # ... ausführen, wenn Abfrage True ist.
+    print("Willkommen auf der Seite der Flensburger Brauerei.")
+# ... ausführen, wenn Abfrage False ist.
+else:
+    print("Weiterleitung zu https://bierbewusstgeniessen.de")
---
+++ VL-Einfuehrung-Programmierung-Vertretung/kontrollstrukturen-while-schleife.py
@@ -0,0 +1,12 @@
+
+# Eintritt im Bierkönig
+gaesteliste = "Erik"
+name = ""
+
+while name != gaesteliste:
+    name = input("Bitte geben Sie Ihren Namen ein: ")
+    
+    if name == gaesteliste:
+        print("Willkommen im Bierkönig.")
+    else:
+        print("Sie stehen leider nicht auf der Gästeliste.")        
