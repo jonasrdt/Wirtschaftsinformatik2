5f62236aab8dca0f8999797d692cff224107a6c9
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-17 15:55:32 +0200

    added exercise 3

---
+++ Uebung-3/lowerupper.py
@@ -0,0 +1,5 @@
+vorname = input("Bitte geben Sie Ihren Vornamen ein: ")
+nachname = input("Bitte geben Sie Ihren Nachnamen ein: ")
+
+print("Der von Ihnen eingegebene Vorname lautet: \t", vorname.capitalize())
+print("Der von Ihnen eingegebene Nachname lautet: \t", nachname.capitalize())
---
+++ Uebung-3/while-schleife.py
@@ -0,0 +1,12 @@
+
+
+# while Schleifen - läuft solange, wie die mit ihr verbundene Bedingung wahr ist
+# und die Abbruchbedingung noch nicht ausgelöst wurde.
+
+arbeitsstunden = 1
+
+while arbeitsstunden <= 5:
+    print("Hallo, ich bin eine while-Schleife. Ich zähle die verstrichenen Arbeitsstunden.")
+    print("Du arbeitest seit", arbeitsstunden, "Stunden.")
+    print("¯\_(ツ)_/¯")
+    arbeitsstunden = arbeitsstunden + 1
