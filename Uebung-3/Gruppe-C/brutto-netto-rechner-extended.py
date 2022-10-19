e42410149f633a934cfe8949f63234c75f9d08a1
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-19 11:05:59 +0200

    added comments to brutto netto rechner

--- Uebung-3/Gruppe-C/brutto-netto-rechner-extended.py
+++ Uebung-3/Gruppe-C/brutto-netto-rechner-extended.py
@@ -1,14 +1,21 @@
-# Definieren der 
+# Definieren der Abbruchbedingung
 korrekte_eingabe = False
+# Starten der while-Schleife, solange noch KEINE korrekte Eingabe getätigt wurde
 while not korrekte_eingabe:
+    # Abfragen des Familienstands des Users
     familienstand = input("Bitte geben Sie Ihren Familienstand an: ")
     print("Der von Ihnen eingegeben Familienstand ist:", familienstand)
+    # Prüfen, ob der familienstand == "ledig" ist 
     if familienstand.lower() == "ledig":
         print("Sie haben angegeben, dass Sie ledig sind.")
+        # Setzen der Abbruchbedingung, da eine korrekte Eingabe getätigt wurde
         korrekte_eingabe = True
+    # Prüfen, ob der familienstand == "verheiratet" ist 
     elif familienstand.lower() == "verheiratet":
         print("Sie haben angegeben, dass Sie verheiratet sind.")
+        # Setzen der Abbruchbedingung, da eine korrekte Eingabe getätigt wurde
         korrekte_eingabe = True
+    # Wenn nichts von den vorherigen zutrifft, soll zu einer neuen Eingabe aufgefordert werden.
     else:
         print("Sie scheinen weder verheiratet noch ledig zu sein. Überprüfen Sie Ihre Eingabe.")
 
