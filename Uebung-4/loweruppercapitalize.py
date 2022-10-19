6ecc49b974d056928e3c00f0f5018079045f1cc3
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-19 10:48:57 +0200

    added brutto netto rechner extended

---
+++ Uebung-4/loweruppercapitalize.py
@@ -0,0 +1,8 @@
+
+
+name = input("Bitte geben Sie Ihren Namen ein:")
+
+print("Jetzt geben wir den Namen mit name.lower() aus \t", name.lower())
+print("Jetzt geben wir den Namen mit name.upper() aus \t", name.upper())
+print("Jetzt geben wir den Namen mit name.capitalize() aus \t", name.capitalize())
+
