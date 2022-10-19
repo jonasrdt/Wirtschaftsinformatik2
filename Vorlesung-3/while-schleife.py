651dc54df4e4fd83bb6e1dc388e0feced2d763c9
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-19 10:01:36 +0200

    added lecture 3

---
+++ Vorlesung-3/while-schleife.py
@@ -0,0 +1,10 @@
+
+
+# Eine While-Schleife läuft immer so lange, wie die mit ihr verknüpfte Beindung wahr ist
+# While-Schleifen werden genutzt, wenn wir die Anzahl der Schleifendurchläufe nicht kennen
+
+entscheidung = "ja"
+
+while entscheidung == "ja":
+    print("Die Schleife läuft durch :-)")
+    entscheidung = input("Soll die Schleife noch Mal laufen (ja/nein): ")
