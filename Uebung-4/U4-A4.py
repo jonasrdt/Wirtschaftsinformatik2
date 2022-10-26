06c3f530e5a1dc240bccfb123c50b9287cfc4de1
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-10-26 10:54:24 +0200

    wip

--- Uebung-4/U4-A4.py
+++ Uebung-4/U4-A4.py
@@ -5,11 +5,13 @@
 
 # Erfassen der Zahlen durch den Nutzer und umwandeln in den Datentypen float
 erste_zahl = float(input("Bitte geben Sie die erste Zahl ein: "))
-zweite_zahl = float(input("Bitte geben Sie die zweite Zahl ein: "))
-
 # Datentyp String für die Variable rechenoperation
 rechenoperation = input("Bitte geben Sie die Rechenoperation ein: ")
+# Erfassen der Zahlen durch den Nutzer und umwandeln in den Datentypen float
+zweite_zahl = float(input("Bitte geben Sie die zweite Zahl ein: "))
 
+
+# Prüfen ob die Rechenoperation zu einer der vorliegenden passt
 if rechenoperation == "+":
     ergebnis = erste_zahl + zweite_zahl
 elif rechenoperation == "-":
@@ -18,5 +20,13 @@ elif rechenoperation == "*":
     ergebnis = erste_zahl * zweite_zahl
 elif rechenoperation == "/":
     ergebnis = erste_zahl / zweite_zahl
-
-print("Bei der Rechnung", erste_zahl, rechenoperation, zweite_zahl, "lautet das Ergebnis:", ergebnis)
+    # … wird ein q eingegeben, endet das Programm.
+elif rechenoperation == "q":
+    print("Sie haben ein q eingegeben. Das Programm endet jetzt.")
+    # … wenn keiner der vorherigen Auswahlmöglichkeiten passt.
+else:
+    print("Ihre Eingabe passt leider zu keiner der Auswahlmöglichkeiten.")
+    
+# Prüfen, ob die Rechenoperation != q ist und bedingtes Ausführen
+if rechenoperation != "q":
+    print("Bei der Rechnung", erste_zahl, rechenoperation, zweite_zahl, "lautet das Ergebnis:", ergebnis)
