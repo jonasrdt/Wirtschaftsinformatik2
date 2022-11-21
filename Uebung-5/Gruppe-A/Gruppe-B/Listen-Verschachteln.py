ad48fcb2fec2b37afe8bbbc45ae217e3e402c87d
Jonas R <jonas.reinhardt@fh-kiel.de>
2022-11-21 15:23:31 +0100

    lists example

--- Uebung-5/Gruppe-A/Gruppe-B/Listen-Verschachteln.py
+++ Uebung-5/Gruppe-A/Gruppe-B/Listen-Verschachteln.py
@@ -1,2 +1,10 @@
+staedte = [["Paris", 2_161_000],          # Index 0
+           ["Cancun", 888_797],           # Index 1
+           ["Kiel", 246_601],             # Index 2
+           ["Göttingen", 116_845]]        # Index 3
 
 
+for stadt in staedte:
+    # In Paris leben 2161000 Menschen
+    print("In", stadt[0], "leben", stadt[1], "Menschen.")
+    
