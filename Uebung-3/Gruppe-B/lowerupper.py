68ae221d44c3cfe901301e823bb6b3d0783468da
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2022-11-28 14:20:52 +0100

    wip

--- Uebung-3/Gruppe-B/lowerupper.py
+++
@@ -1,7 +0,0 @@
-
-
-name = input("Bitte geben Sie Ihren Namen ein: ")
-
-print("Der Name wurde angepasst mit lower(): ", name.lower())
-print("Der Name wurde angepasst mit upper(): ", name.upper())
-print("Der Name wurde angepasst mit capitalize(): ", name.capitalize())
--- Uebung-6/Gruppe-A/dict-schleife.py
+++
@@ -1,8 +0,0 @@
-count = 1
-dict = {}
-def c():
-    for counter in range(3):
-        dict.update({"Counter": counter})
-    return counter
-c()
-print(dict["Counter"])
