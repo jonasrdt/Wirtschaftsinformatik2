b812568d15a840316c95664d09c71089daa23da4
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-03-21 11:38:44 +0100

    Erste Vorlesung

---
+++ Vorlesung-1/Rechnen-mit-Variablen.py
@@ -0,0 +1,11 @@
+
+# Datentyp: float
+mwst = 0.16
+
+# Datentyp: integer
+sennheiser_kopfhoerer = 120
+
+# Datentyp: float
+sennheiser_kopfhoerer_mit_mwst = sennheiser_kopfhoerer * (1+mwst) 
+
+print(sennheiser_kopfhoerer_mit_mwst)
---
+++ Vorlesung-1/Variablendefinition.py
@@ -0,0 +1,19 @@
+
+
+# Variablennamen: vorname, nachname
+# Datentyp: String (Zeichenkette)
+# Variableninhalte: "Jonas", "Reinhardt"
+vorname = "Jonas"
+nachname = "Reinhardt"
+
+# Variablennamen: alter, groesse
+# Datentyp: Integer (ganze Zahlen)
+# Variableninhalte: 30, 192
+alter = 30
+groesse = 192
+
+# Variablennamen: arbeitsweg
+# Datentyp: Float (Fließkommazahlen)
+# Variableninhalte: 14.5
+arbeitsweg_in_km = 14.5
+
