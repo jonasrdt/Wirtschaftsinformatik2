d3d2c9be420f36e5045e5b794c9e190ee8d6520a
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-03-27 09:08:01 +0200

    geburtsjahr aufgabe

---
+++ Uebung-1/Gruppe-A/geburtsjahr-alter.py
@@ -0,0 +1,8 @@
+
+
+# Schreiben Sie ein kleines Python-Programm, das auf Basis des Geburtsjahres
+# berechnet, wie alt die Person heute ist.
+# Beispiel:
+# Wann bist du geboren: 1992
+# Wenn du 1992 geboren bist, bist du heute 31 Jahre alt.
+
