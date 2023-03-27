22b667b0d04c05510f05fabd3edf755d732c3d2c
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-03-27 09:05:09 +0200

    Uebung-1

---
+++ Uebung-1/Gruppe-A/Datentypen.py
@@ -0,0 +1,7 @@
+
+
+alter = 30
+alter = float(alter)
+print(type(alter))
+
+print(alter)
---
+++ Uebung-1/Gruppe-A/input-output.py
@@ -0,0 +1,16 @@
+print("Hallo. Ich möchte gerne wissen, wie dein Name ist. :-)")
+
+# Erfasst Inhalt mit dem Datentypen 'string' in der Variable name
+name = input("Name eingeben: ")
+print("Der eingegebene Name lautet: ", name)
+
+# Ergänzen Sie das Programm so, dass auch noch der Alter und die Größe erfasst
+# und in einer Variable gespeichert werden.
+
+# Erfasst Inhalt mit dem Datentypen 'integer' in der Variable alter
+alter = int(input("Bitte gib dein Alter ein: "))
+print("Das eingegebene Alter lautet:", alter)
+
+# Erfasst Inhalt mit dem Datentypen 'float' in der Variable groesse
+groesse = float(input("Bitte gib deine Größe ein: "))
+print("Die eingegebene Größe ist:", groesse)
