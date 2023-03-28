a969ae33107678b7578d2cac9673932e320b5e59
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-03-28 11:11:47 +0200

    Teile erste Vorlesung

---
+++ Vorlesung-2/variablendefinition.py
@@ -0,0 +1,13 @@
+
+
+# Definieren der Variable 'alter' und Zuweisen des Wertes '20'
+# Datentypen: Integer (int) => ganze Zahlen
+alter = int(input("Bitte geben Sie Ihr Alter ein: "))
+print("Ihr Alter lautet:", alter)
+print("In 50 Jahren sind Sie:", alter + 50, "Jahre alt.")
+
+# Definieren der Variable 'stundenlohn_in_euro'
+# Datentyp: Float (float) => Fließkommazahlen
+stundenlohn_in_euro = float(input("Bitte geben Sie Ihren Stundenlohn in Euro ein: "))
+print("Ihr Stundenlohn beträgt:", stundenlohn_in_euro, "€.")
+print("Wäre auch cool, wenn der Stundenlohn:", stundenlohn_in_euro + 2, "€ betrüge.")
