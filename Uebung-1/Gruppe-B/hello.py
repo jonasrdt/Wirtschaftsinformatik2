2b94473d28e19a085b0647d151c42cce5ca9c89c
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-03-28 11:11:48 +0200

    Teile erste Vorlesung

---
+++ Uebung-1/Gruppe-B/hello.py
@@ -0,0 +1,11 @@
+
+
+print("Moin, ich bin ein Python Programm.")
+
+name = input("Sag mir deinen Namen: ")
+print("Hallo", name)
+
+geburtsjahr = input("Sag mir dein Geburtsjahr: ")
+print("Du bist", geburtsjahr, "geboren.")
+
+groesse = input("Wie groß bist du: ")
---
+++ Vorlesung-2/escapecodes.py
@@ -0,0 +1,8 @@
+
+
+# Nachfolgend wird der QM Preis mit zwei Tabstopps ausgegeben
+print("Anzahl qm: \t \t 68")
+
+# It prints the string "Nebenkosten pro qm: \t 1.50€"
+print("Nebenkosten pro qm: \t 1.50€")
+
