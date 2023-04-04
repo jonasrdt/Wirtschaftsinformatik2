f0ba3d865b305b6615c14c192551321c4260a2af
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-04-04 12:39:12 +0200

    added lecture 3

--- Vorlesung-3/Zufallsgeneratorspiel.py
+++ Vorlesung-3/Zufallsgeneratorspiel.py
@@ -3,13 +3,17 @@ import random
 zahl_1 = random.randint(1,10)
 zahl_2 = random.randint(1,10)
 loesung = zahl_1 + zahl_2
+# Definieren der Variable 'richtige_antwort' vom Datentypen Boolean
+richtige_antwort = False
 
 print("Willkommen zum Zufallsgeneratorspiel.")
 print("Hier ist eine schwierige Mathe-Aufgabe für dich. Findest du die Lösung?")
 print("Hier die Aufgabe:", zahl_1, "+", zahl_2)
-user_loesung = int(input("Bitte gib hier deine Lösung ein: "))
 
-if user_loesung == loesung:
-    print("Glückwunsch! Das Ergebnis ist richtig:", user_loesung)
-else:
-    print("Das war leider falsch!")
+while not richtige_antwort:
+    user_loesung = int(input("Bitte gib hier deine Lösung ein: "))
+    if user_loesung == loesung:
+        print("Glückwunsch! Das Ergebnis ist richtig:", user_loesung)
+        richtige_antwort = True
+    else:
+        print("Das war leider falsch!")
