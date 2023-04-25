55504145b0382ebafda2cd506636c614b050fd18
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-04-25 11:29:52 +0200

    added code for cuntions

--- Vorlesung-5/Funktionen-Mittelwert.py
+++ Vorlesung-5/Funktionen-Mittelwert.py
@@ -1,6 +1,16 @@
 
-# Definition der Funktion
+
 def mittelwertzweierzahlen(zahl1, zahl2):
+    """
+    The function calculates the average of two given numbers and returns the result.
+    
+    :param zahl1: The first number that will be used to calculate the average
+    :param zahl2: The parameter 'zahl2' is a variable that represents the second number that will be
+    used to calculate the average (or mean) with the first number ('zahl1') in the
+    'mittelwertzweierzahlen' function
+    :return: the average (or mean) of two numbers, which is calculated by adding the two numbers and
+    dividing the sum by 2.
+    """
     # Definieren der lokalen Variable 'mittelwert'
     mittelwert = (zahl1 + zahl2) / 2
     # Wiedergeben/Herausgeben des Wertes für 'mittelwert'
---
+++ Vorlesung-5/Funktionen-Rechner-return.py
@@ -0,0 +1,14 @@
+
+def calculatesum(zahl1, zahl2):
+    """
+    The function "calculatesum" takes two numbers as input, adds them together, and returns the result.
+    
+    :param zahl1: The first number to be added in the calculation
+    :param zahl2: The parameter "zahl2" is a variable that represents the second number that will be
+    added to the first number (represented by the parameter "zahl1") in the function "calculatesum"
+    :return: the sum of the two input numbers (zahl1 and zahl2).
+    """
+    return zahl1 + zahl2
+
+print(calculatesum(5,10))
+
