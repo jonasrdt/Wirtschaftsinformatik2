a0d978af04b1fa6c58c562fd1f276e99c1a85d30
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-06-05 09:52:13 +0200

    added replace example

--- Uebung-8/Gruppe-A/Replace.py
+++ Uebung-8/Gruppe-A/Replace.py
@@ -1,2 +1,9 @@
 
 
+sonderzeichen = [',', '.', '"', '!', '?', ':', '-', '#', '%', '&', '/', ' ', '0', '1']
+satz = "Der#Hund!läuft.in-die Küche."
+
+for element in sonderzeichen:
+    satz = satz.replace(element, "")
+
+print(satz)
